615e6a6b1696dfaa99b059ed2b56bf09f3b0b29a
adarshrai67 <officialadarsh67@gmail.com>
2024-09-12 14:33:40 +0530

    addding LTDS.py

---
+++ LTDS.py
@@ -0,0 +1,5 @@
+list = ["Adarsh", "Vandana", "Abhishek"]
+tuple = ("adarsh", "vandana", "abhishek")
+dict = {"name" : "adarsh", "roll" : 22210}
+set = {"ADARSH", "VANDANA", "ABHISHEK"}
+print(list,"\n",tuple,"\n",dict,"\n",set)
