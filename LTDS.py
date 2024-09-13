aa45ddea792b1e97bbddb50c3de609d725f0971e
adarshrai67 <officialadarsh67@gmail.com>
2024-09-13 10:24:10 +0530

    Updating

--- LTDS.py
+++ LTDS.py
@@ -1,12 +1,11 @@
 list = ["Adarsh", "Vandana", "Abhishek"]
+list1 = [6,7,67,76]
 tuple = ("adarsh", "vandana", "abhishek")
 dict = {"name" : "adarsh", "roll" : 22210}
 set = {"ADARSH", "VANDANA", "ABHISHEK"}
 print(list,"\n",tuple,"\n",dict,"\n",set)
 print (len(list))
-list1 = [6,7,67,76]
-avg = sum(list1)/len(list1)
-print (avg)
+print (sum(list1)/len(list1))
 list1.append(5)
 print(list1)
 list1.remove(5)
